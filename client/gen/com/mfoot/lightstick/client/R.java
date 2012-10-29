63d0974d068a247bf804a8cdaba4a51b929c108c
Martin Foot <martin@mfoot.com>
2012-10-29 23:16:00 +0000

    Very early work on UI mockup.

--- client/gen/com/mfoot/lightstick/client/R.java
+++ client/gen/com/mfoot/lightstick/client/R.java
@@ -11,30 +11,37 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static int padding_large=0x7f040002;
-        public static int padding_medium=0x7f040001;
-        public static int padding_small=0x7f040000;
+        public static final int padding_large=0x7f040002;
+        public static final int padding_medium=0x7f040001;
+        public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static int ic_action_search=0x7f020000;
-        public static int ic_launcher=0x7f020001;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static int menu_settings=0x7f080000;
+        public static final int edit_menu_button=0x7f080003;
+        public static final int gallery_menu_button=0x7f080004;
+        public static final int menu_settings=0x7f080005;
+        public static final int play_menu_button=0x7f080002;
+        public static final int settings_menu_button=0x7f080000;
+        public static final int wireless_connection=0x7f080001;
     }
     public static final class layout {
-        public static int activity_main=0x7f030000;
+        public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static int activity_main=0x7f070000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static int app_name=0x7f050000;
-        public static int hello_world=0x7f050001;
-        public static int menu_settings=0x7f050002;
-        public static int title_activity_main=0x7f050003;
+        public static final int app_name=0x7f050000;
+        public static final int content_settings_button=0x7f050004;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
+        public static final int wireless_connection=0x7f050005;
     }
     public static final class style {
-        public static int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f060000;
     }
 }
